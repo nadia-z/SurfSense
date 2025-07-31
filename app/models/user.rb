2314fad2e42fd6a4d7f7ab1989f739f6e7722b53
eitan hoyte <eitanhoyte12@gmail.com>
2025-07-31 16:58:24 +0100

    added back functions that were rolled back

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ApplicationRecord
+  before_validation :strip_db
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :locations
   has_many :selected_forecasts
+
+  has_one_attached :photo
+
+  private
+
+  def strip_db
+    self.first_name = nil if first_name.blank?
+    self.last_name  = nil if last_name.blank?
+    self.photo = nil if photo.blank?
+  end
 end
