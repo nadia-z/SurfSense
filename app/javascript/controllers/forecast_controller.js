c959829b55854e296c0367aef5ae27913bf9b2f9
Nadia Zeissig <33801529+nadia-z@users.noreply.github.com>
2025-08-04 14:31:15 +0200

    flowchartconditions

--- app/javascript/controllers/forecast_controller.js
+++ app/javascript/controllers/forecast_controller.js
@@ -1,143 +1,147 @@
-import { Controller } from "@hotwired/stimulus"
-import { createDefs, applyBlur, removeBlur, addPaddingRect } from "../flowchart_effects"
-import { initializeNodeClickListeners } from "../flowchart_interactions"
-import { findAnswerToSwellHeight, updateSwellHeightAnswer } from "../swell_height_answers";
+  import { Controller } from "@hotwired/stimulus"
+  import { createDefs, applyBlur, removeBlur, addPaddingRect } from "../flowchart_effects"
+  import { initializeNodeClickListeners } from "../flowchart_interactions"
+  import { findAnswerToSwellHeight, updateSwellHeightAnswer } from "../swell_height_answers";
 
-// Connects to data-controller="forecast"
-export default class extends Controller {
-  static targets = ["card"]
+  // Connects to data-controller="forecast"
+  export default class extends Controller {
+    static targets = ["card"]
 
-  connect() {
-<<<<<<< Updated upstream
-    console.log("Forecast controller connected")
-  }
-=======
-  console.log("Forecast controller connected");
+    connect() {
+    console.log("Forecast controller connected");
 
-  document.addEventListener("timeSlot:selected", (event) => {
-    const { swellHeight } = event.detail;
-    this.showFlowchartWithUpdatedData(swellHeight);
-  });
-}
->>>>>>> Stashed changes
-
-  selectCard(event) {
-    event.preventDefault()
-    const selectedCard = this.cardTarget
-    // Placeholder for logic
+    document.addEventListener("timeSlot:selected", (event) => {
+      const { swellHeight } = event.detail;
+      this.showFlowchartWithUpdatedData(swellHeight);
+    });
   }
 
-  toggleLongForecastInfo(event) {
-    const forecastContainer = document.getElementById("forecast-container")
-    console.log("we are in toggleForecastInfo now")
-    forecastContainer.style.display = "grid"
+    selectCard(event) {
+      event.preventDefault()
+      const selectedCard = this.cardTarget
+      // Placeholder for logic
+    }
 
-    const buttonBack = document.getElementById("btn-back")
-     if (buttonBack) {
-       buttonBack.style.display = "block"
-     }
-  }
+    toggleLongForecastInfo(event) {
+      const forecastContainer = document.getElementById("forecast-container")
+      console.log("we are in toggleForecastInfo now")
+      forecastContainer.style.display = "grid"
 
-  focusOnCard(event) {
-    console.log("we are in focusOnCard now")
-    const cardsContainer = document.getElementById("break-cards-container")
-    cardsContainer.innerHTML = ""
-    cardsContainer.insertBefore(event.currentTarget.parentElement, null)
+      const buttonBack = document.getElementById("btn-back")
+      if (buttonBack) {
+        buttonBack.style.display = "block"
+      }
+    }
 
-    this.toggleLongForecastInfo(event)
-    this.deactivateClickFunctionalities(event)
-  }
+    focusOnCard(event) {
+      console.log("we are in focusOnCard now")
+      const cardsContainer = document.getElementById("break-cards-container")
+      cardsContainer.innerHTML = ""
+      cardsContainer.insertBefore(event.currentTarget.parentElement, null)
 
-  deactivateClickFunctionalities() {
-    //I want to deactivate the card click functionalities in order to click on other buttons in the card
-    // without triggering the 'focusOnCard' function
-    // Find ALL elements with this action and remove the data-action attribute
-    const elementToDeactivate = document.querySelector('#card-template')
-    elementToDeactivate.removeAttribute('data-action')
+      this.toggleLongForecastInfo(event)
+      this.deactivateClickFunctionalities(event)
+    }
 
-    console.log('DONE - Deactivated')
-  }
+    deactivateClickFunctionalities() {
+      //I want to deactivate the card click functionalities in order to click on other buttons in the card
+      // without triggering the 'focusOnCard' function
+      // Find ALL elements with this action and remove the data-action attribute
+      const elementToDeactivate = document.querySelector('#card-template')
+      elementToDeactivate.removeAttribute('data-action')
 
-  showFlowchartWithUpdatedData(swellHeight) {
-    console.log("Flowchart triggered from time slot");
-    console.log("swellHeight passed in:", swellHeight);
+      console.log('DONE - Deactivated')
+    }
+
+    showFlowchartWithUpdatedData(swellHeight) {
+
+        const breakCardContainer = document.getElementById("break-cards-container");
+        if (breakCardContainer) {
+          breakCardContainer.innerHTML = ""; // clear cards
+          breakCardContainer.style.display = "none";
+        }
 
 
-    const flowchartContainer = document.getElementById("flowchart-container")
-    flowchartContainer.style.display = "block";
-    flowchartContainer.style.width = "100%";
-    flowchartContainer.style.height = "auto";
-    flowchartContainer.style.minHeight = "600px";
-    const svgContainer = document.getElementById("svg-container")
+      fetch("/flowchart.svg")
+        .then(res => res.text())
+        .then(svgText => {
+          console.log("fetching svg")
+          const parser = new DOMParser()
+          const svgDoc = parser.parseFromString(svgText, "image/svg+xml")
+          const parsedSvg = svgDoc.documentElement
 
-     if (flowchartContainer.dataset.loaded === "true") {
-    console.log("SVG already loaded, skipping fetch");
-    return;
-  }
+          const svg = document.querySelector("svg")
+          document.body.appendChild(parsedSvg);
 
-    fetch("/flowchart.svg")
-      .then(res => res.text())
-      .then(svgText => {
-        svgContainer.innerHTML = svgText
-        console.log(svgContainer.innerHTML)
-        flowchartContainer.dataset.loaded = "true"
-        flowchartContainer.style.display = "block"
-        const svg = document.querySelector("svg")
+          let swellHeightEl = svg.getElementById("swell-value");
 
-        createDefs(svg)
-        const edges = svg.querySelectorAll('[id^="e-"]')
-        const nodes = svg.querySelectorAll('[id^="sn-"], [id^="qsn-"]')
-        const swellTextEl = svg.getElementById('swell-height-value')
-        const swellAnswerEl = svg.getElementById('swell-height-answer')
+          createDefs(parsedSvg)
+          const edges = parsedSvg.querySelectorAll('[id^="e-"]')
+          const nodes = parsedSvg.querySelectorAll('[id^="sn-"], [id^="qsn-"]')
 
-        this.hideAllElements(edges, nodes)
-        this.showNodesAndEdges(nodes, svg, [0, 1, 2, 3])
-        this.blurNodes(nodes, [4, 8])
-        initializeNodeClickListeners(nodes, edges, svg, { applyBlur, removeBlur })
-        console.log("swellHeight is:")
-        console.log(swellHeight)
-        updateSwellHeightAnswer(swellAnswerEl, swellHeight)
+          this.hideAllElements(edges, nodes)
+          this.showNodesAndEdges(nodes, parsedSvg, [0, 1, 2, 3])
+          this.blurNodes(nodes, [4, 8])
+          initializeNodeClickListeners(nodes, edges, parsedSvg, { applyBlur, removeBlur })
+          this.updateSwellHeightText(swellHeightEl, swellHeight)
+          console.log("swellHeight is:")
+          console.log(swellHeight)
+        })
+    }
+
+    hideAllElements(edges, nodes) {
+      edges.forEach(edge => {
+        edge.style.visibility = "hidden"
+        edge.setAttribute("stroke-width", "1")
+        edge.setAttribute("stroke", "#D8E6E7")
       })
-  }
-
-  hideAllElements(edges, nodes) {
-    edges.forEach(edge => {
-      edge.style.visibility = "hidden"
-      edge.setAttribute("stroke-width", "1")
-      edge.setAttribute("stroke", "#D8E6E7")
-    })
-    nodes.forEach(node => {
-      node.style.visibility = "hidden"
-      addPaddingRect(node)
-    })
-  }
-
-  showNodesAndEdges(nodes, svg, nodeIndices) {
-    nodeIndices.forEach(index => {
-      const node = nodes[index]
-      if (!node) return
-
-      node.style.visibility = "visible"
-      const nodeId = node.id.replace(/^q?sn-/, "")
-      const relatedEdges = svg.querySelectorAll(`[id^="e-${nodeId}-"]`)
-
-      relatedEdges.forEach(edge => {
-        edge.style.visibility = "visible"
-        const toId = edge.id.split("-")[2]
-        const teasedNode = svg.getElementById(`sn-${toId}`) || svg.getElementById(`qsn-${toId}`)
-        if (teasedNode) teasedNode.style.visibility = "visible"
+      nodes.forEach(node => {
+        node.style.visibility = "hidden"
+        addPaddingRect(node)
       })
-    })
+    }
+
+    showNodesAndEdges(nodes, svg, nodeIndices) {
+      nodeIndices.forEach(index => {
+        const node = nodes[index]
+        if (!node) return
+
+        node.style.visibility = "visible"
+        const nodeId = node.id.replace(/^q?sn-/, "")
+        const relatedEdges = svg.querySelectorAll(`[id^="e-${nodeId}-"]`)
+
+        relatedEdges.forEach(edge => {
+          edge.style.visibility = "visible"
+          const toId = edge.id.split("-")[2]
+          const teasedNode = svg.getElementById(`sn-${toId}`) || svg.getElementById(`qsn-${toId}`)
+          if (teasedNode) teasedNode.style.visibility = "visible"
+        })
+      })
+    }
+
+    blurNodes(nodes, nodeIndices) {
+      nodeIndices.forEach(index => {
+        const node = nodes[index]
+        if (!node) return
+
+        node.style.visibility = "visible"
+        node.setAttribute("filter", "url(#blur-effect)")
+        node.style.pointerEvents = "none"
+      })
+    }
+
+
+    updateSwellHeightText(swellHeightEl, swellHeight) {
+    if (!swellHeightEl || !swellHeight) return;
+    const newText = document.createElementNS("http://www.w3.org/2000/svg", "text")
+    newText.setAttribute("id", "swell-value")
+    newText.setAttribute("x", "110")
+    newText.setAttribute("y", "400")
+    newText.setAttribute("style", "font-family: 'Self Modern', sans-serif; font-size: 1.4rem; fill: black;");
+    newText.textContent = `${swellHeight.toFixed(1)} m`;
+    console.log("newText")
+    console.log(newText)
+    swellHeightEl.parentNode.replaceChild(newText, swellHeightEl)
   }
 
-  blurNodes(nodes, nodeIndices) {
-    nodeIndices.forEach(index => {
-      const node = nodes[index]
-      if (!node) return
-
-      node.style.visibility = "visible"
-      node.setAttribute("filter", "url(#blur-effect)")
-      node.style.pointerEvents = "none"
-    })
   }
-}
--- app/javascript/controllers/time_slots_controller.js
+++ app/javascript/controllers/time_slots_controller.js
@@ -8,66 +8,16 @@ export default class extends Controller {
     console.log("TimeSlots controller connected")
   }
 
-<<<<<<< Updated upstream
   selectTimeSlot(event) {
     const clickedSlot = event.currentTarget
 
-    // Hide all other time-slot-grid divs except clicked one
-    this.timeSlotTargets.forEach(slot => {
-      if (slot === clickedSlot) {
-        slot.style.display = "flex"  // or "" to reset
-      } else {
-        slot.style.display = "none"
-      }
-    })
-=======
-// Hide all slots also the card except the clicked one, append time-slot-grid element to
-// the DOM, inside a wrapper that is created in order to display it properly styled on the left side of the screen
-selectTimeSlot(event) {
-  const clickedSlot = event.currentTarget;
-  console.log(`time slot "${clickedSlot}" selected`);
+    console.log(`time slot "${clickedSlot}" selected`);
 
-  const allSlots = document.querySelectorAll('[data-time-slots-target="timeSlot"]');
+    const swellEl = clickedSlot.querySelector('[data-weather="swell-height"]');
+    const swellHeight = parseFloat(swellEl?.dataset.swellHeight || "0");
 
-  // Hide all slots except the clicked one
-  allSlots.forEach(slot => {
-    slot.style.display = (slot === clickedSlot) ? "flex" : "none";
-    if (slot === clickedSlot) {
-      slot.style.flex = "none"; // or ""
-    }
-  });
->>>>>>> Stashed changes
-
-  // Find the parent break cards container
-  const breakCardContainer = clickedSlot.closest('#break-cards-container');
-
-  if (breakCardContainer) {
-    // Hide the whole container (including other break  cards)
-    breakCardContainer.style.display = 'none';
-
-   // Create or get the time-slot-wrapper
-    let wrapper = document.getElementById('time-slot-wrapper');
-    if (!wrapper) {
-      wrapper = document.createElement('div');
-      wrapper.id = 'time-slot-wrapper';
-      document.body.appendChild(wrapper);
-    }
-
-    // Clear previous content and append the clicked slot
-    wrapper.innerHTML = '';
-    wrapper.appendChild(clickedSlot);
-
-    clickedSlot.style.display = 'flex';
-    clickedSlot.style.flex = 'none'; // or 'auto'
-    clickedSlot.style.width = '100%'; // fill wrapper width but not more
-  }
-
-  // Dispatch event with swellHeight info to be used in forecast controller method in switchToFlowChartFromTimeSlot
-  const swellEl = clickedSlot.querySelector('[data-weather="swell-height"]');
-  const swellHeight = parseFloat(swellEl?.dataset.swellHeight || "0");
-
-  document.dispatchEvent(new CustomEvent("timeSlot:selected", {
-    detail: { swellHeight }
-  }));
+    document.dispatchEvent(new CustomEvent("timeSlot:selected", {
+      detail: { swellHeight }
+    }));
 }
 }
